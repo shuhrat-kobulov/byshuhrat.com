b6bc1076c67d118830c6b792dc8ac09e088d9a5c
shuhrat <shukhratbekqobulov@gmail.com>
2026-06-16 11:45:58 +0500

    feat: add generateStaticParams function to read directories for dynamic slugs

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -1,3 +1,14 @@
+import { readdir } from 'fs/promises';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return <div className="max-w-2xl mx-auto">{children}</div>;
+}
+
+export async function generateStaticParams() {
+    const entries = await readdir('./public/', { withFileTypes: true });
+    const dirs = entries
+        .filter((entry) => entry.isDirectory())
+        .filter((entry) => !entry.name.startsWith('_'))
+        .map((entry) => entry.name);
+    return dirs.map((dir) => ({ slug: dir }));
 }
